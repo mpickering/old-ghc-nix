8db0fb6878c1393be017794fb1f1232e7857667a
Matthew Pickering <matthewtpickering@gmail.com>
2019-06-10 16:13:16 +0100

    WIP: Use artifact.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
 { pkgs }:
-let mkGhc = pkgs.callPackage ./ghc.nix {};
+let mkGhc = pkgs.callPackage ./artifact.nix {};
     hashes = builtins.fromJSON (builtins.readFile ./hashes.json);
+    mkTarball = { url, hash, ...}: pkgs.fetchurl { url = url;
+                                                       sha256 = hash;
+                                                     };
 in
-  builtins.mapAttrs (_: v: mkGhc v) hashes // { inherit mkGhc; }
+  builtins.mapAttrs (_: v: mkGhc { bindistTarball = (mkTarball v); ncursesVersion = "6";}) hashes // { inherit mkGhc; }
